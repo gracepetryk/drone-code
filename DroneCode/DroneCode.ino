291c86874ecd243d339bc31887d19a4de5f0073e
John Petryk <signmeup1999@gmail.com>
2016-05-13 08:02:16 -0400

    minor code changes

--- DroneCode/DroneCode.ino
+++ DroneCode/DroneCode.ino
@@ -8,8 +8,6 @@ int frontMotorPin = 1;
 int backMotorPin = 2;
 int rightMotorPin = 3;
 int leftMotorPin = 4;
-int gyroPin = A0; //gyroscope
-int accelPin = A1; //accelerometer
 
 //pid perameters
 double pitchSetpoint, pitchInput, pitchOutput; // pitch PID perameters 
@@ -17,11 +15,13 @@ double yawSetpoint, yawInput, yawOutput; // yaw PID perameters
 double rollSetpoint, rollInput, rollOutput; // roll PID perameters
 double altitudeSetpoint, altitudeInput, altitudeOutput; // altitude PID perameters
 
-//PID objects - &symbols for global varables
+//PID objects - & symbols for global varables
 PID pitchPID(&pitchInput, &pitchOutput, &pitchSetpoint, 1, 1, 1, DIRECT);
 PID yawPID(&yawInput, &yawOutput, &yawSetpoint, 1, 1, 1, DIRECT);
 PID rollPID(&rollInput, &rollOutput, &rollSetpoint, 1, 1, 1, DIRECT);
 
+double PIDmin = 0.5;
+double PIDmax = 1.5;
 
 double powerMultiplier = 0;
 
@@ -32,23 +32,26 @@ void setup()
     pinMode(backMotorPin, OUTPUT);
     pinMode(rightMotorPin, OUTPUT);
     pinMode(leftMotorPin, OUTPUT);
-    pinMode(gyroPin, INPUT);
-    pinMode(accelPin, INPUT);
 
     // intialize PID's
     pitchPID.SetMode(AUTOMATIC);
     yawPID.SetMode(AUTOMATIC);
     rollPID.SetMode(AUTOMATIC);
-}
 
+    //set PID limits
+    pitchPID.SetOutputLimits(PIDmin, PIDmax);
+    yawPID.SetOutputLimits(PIDmin, PIDmax);
+    rollPID.SetOutputLimits(PIDmin, PIDmax);
+}
 
 void loop() 
 {
-    pitchPID.Compute();
-    yawPid.Compute();
-    rollPID.Compute();
-    
+    //TODO: add gyro and acclerometer code
 
+    // These will return ratios of how much to differentiate the power levels in different motors
+    pitchPID.Compute();
+    yawPID.Compute();
+    rollPID.Compute();
 }
 
 
