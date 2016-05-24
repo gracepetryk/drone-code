12327a6274ffdb5e3f79dfaa18270d5d25d173bc
peachoftree <signmeup1999@gmail.com>
2016-05-24 11:37:41 -0400

    MOAR LIBRARIES

--- DroneCode/DroneCode.ino
+++ DroneCode/DroneCode.ino
@@ -1,25 +1,31 @@
+#include <Adafruit_Sensor.h>
+#include <Adafruit_LSM9DS0.h>
 #include <MadgwickAHRS.h>
 #include <PID_AutoTune_v0.h>
 #include <Servo.h>
 #include <PID_v1.h>
+#include <Wire.h>
+#include <SPI.h>
 
 /* Authors: John Petryk and Henryk Viana
  * Arduino code for running the drone
  */
 
 //pins
-Servo frontMotor
-Servo backMotor
-Servo rightMotor
-Servo leftMotor
+Servo frontMotor;
+Servo backMotor;
+Servo rightMotor;
+Servo leftMotor;
 
 Madgwick filter;
 
+Adafruit_LSM9DS0 lsm = Adafruit_LSM9DS0();
+
 //pid perameters
-float pitchSetpoint, pitchInput, pitchOutput; // pitch PID perameters 
-float yawSetpoint, yawInput, yawOutput; // yaw PID perameters
-float rollSetpoint, rollInput, rollOutput; // roll PID perameters
-float altitudeSetpoint, altitudeInput, altitudeOutput; // altitude PID perameters
+double pitchSetpoint, pitchInput, pitchOutput; // pitch PID perameters 
+double yawSetpoint, yawInput, yawOutput; // yaw PID perameters
+double rollSetpoint, rollInput, rollOutput; // roll PID perameters
+double altitudeSetpoint, altitudeInput, altitudeOutput; // altitude PID perameters
 
 //PID objects - & symbols for global varables
 PID pitchPID(&pitchInput, &pitchOutput, &pitchSetpoint, 1, 0, 0, DIRECT);
@@ -27,7 +33,7 @@ PID yawPID(&yawInput, &yawOutput, &yawSetpoint, 1, 0, 0, DIRECT);
 PID rollPID(&rollInput, &rollOutput, &rollSetpoint, 1, 0, 0, DIRECT);
 
 float PIDmin = 1;
-float PIDmax = sqrt(2);
+float PIDmax = sqrt(1.5);
 
 float powerMultiplier = 0; // min 0, max 1
 
@@ -48,25 +54,27 @@ void setup()
     pitchPID.SetOutputLimits(PIDmin, PIDmax);
     yawPID.SetOutputLimits(PIDmin, PIDmax);
     rollPID.SetOutputLimits(PIDmin, PIDmax);
+
+    lsm.begin();
+
+    filter.begin(25);
 }
 
 
 void applyMotorPower(PID pid, Servo primaryMotor, Servo secondaryMotor)
 {
-    int primaryPower = map(powerMultiplier*pid.Compute(), 0, sqrt(2), 30, 130);
-    int secondaryPower = map(powerMultiplier/pid.Compute(), 0, 1/sqrt(2), 30, 130);
+    int primaryPower = map(powerMultiplier*pid.Compute(), 0f, PIDmax, 30f, 130f);
+    int secondaryPower = map(powerMultiplier/pid.Compute(), 0f, 1/PIDmax, 30f, 130f);
     primaryMotor.write(primaryPower);
-    secondaryMotor.write(seccondaryPower)
+    secondaryMotor.write(secondaryPower);
 }
 
+void applyYaw
 
 
 void loop() 
 {
-    //TODO: add gyro and acclerometer code
-
-    // These will return ratios of how much to differentiate the power levels in different motors
-    pitchPID.Compute();
-    yawPID.Compute();
-    rollPID.Compute();
+    applyMotorPower(pitchPID, frontMotor, backMotor);
+    applyMotorPower(rollPid, frontMotor, backMotor);
+    
 }
